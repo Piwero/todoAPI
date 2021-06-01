b6dd891eeef8027ab541b042da0a6a65a7c8db3d
Piwero <piwero@gmail.com>
2021-06-01 18:01:14 +0100

    update poetry with flake9

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price_currency = models.CharField(max_length=3)
+    sku = models.CharField(max_length=20)
+    total_sold = models.PositiveIntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
